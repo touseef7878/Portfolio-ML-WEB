93564105484ea18d1d213da7a9d282ba562fe240
Touseef Ur Rehman <touseefurrehman5554@gmail.com>
2026-02-24 15:43:07 +0500

    updated colour scheme

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -14,9 +14,9 @@ const Index = () => {
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden relative">
       {/* Floating animated gradient orbs */}
       <div className="fixed inset-0 overflow-hidden pointer-events-none z-0">
-        <div className="absolute top-1/4 left-1/4 w-96 h-96 bg-primary/20 rounded-full blur-[100px] animate-float" />
-        <div className="absolute top-1/2 right-1/4 w-80 h-80 bg-accent/15 rounded-full blur-[100px] animate-float-delayed" />
-        <div className="absolute bottom-1/4 left-1/3 w-72 h-72 bg-purple-500/10 rounded-full blur-[100px] animate-float-slow" />
+        <div className="absolute top-1/4 left-1/4 w-96 h-96 bg-cyan-500/20 rounded-full blur-[100px] animate-float" />
+        <div className="absolute top-1/2 right-1/4 w-80 h-80 bg-blue-500/15 rounded-full blur-[100px] animate-float-delayed" />
+        <div className="absolute bottom-1/4 left-1/3 w-72 h-72 bg-cyan-400/10 rounded-full blur-[100px] animate-float-slow" />
       </div>
 
       <div className="relative z-10">
