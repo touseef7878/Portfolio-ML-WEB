f9e39ef2556214babcaf806649e0534a742678cc
Touseef Ur Rehman <touseefurrehman5554@gmail.com>
2026-08-27 11:45:51 +0500

    Updated projects overall cleaned

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,12 @@ import { useState, useEffect } from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "next-themes";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Loader from "./components/Loader";
 import AiAssistant from "./components/AiAssistant";
-
-const queryClient = new QueryClient();
 
 const App = () => {
   const [loading, setLoading] = useState(true);
@@ -31,19 +28,17 @@ const App = () => {
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
       <div className={showContent ? "fade-in" : "opacity-0"}>
-        <QueryClientProvider client={queryClient}>
-          <TooltipProvider>
-            <Toaster />
-            <Sonner />
-            <BrowserRouter>
-              <Routes>
-                <Route path="/" element={<Index />} />
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-              <AiAssistant />
-            </BrowserRouter>
-          </TooltipProvider>
-        </QueryClientProvider>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+            <AiAssistant />
+          </BrowserRouter>
+        </TooltipProvider>
       </div>
     </ThemeProvider>
   );
